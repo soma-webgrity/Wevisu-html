9afd0dec0a9b38127e4a248d7ecf231fb44461bd
soma-webgrity <85211691+soma-webgrity@users.noreply.github.com>
2021-06-02 10:13:37 +0530

    Add files via upload
    Add Comment wevisu html:

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+/*function openTab(evt, servicename) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(servicename).style.display = "block";
+  evt.currentTarget.className += " active";
+}*/
+
+$(document).ready(function(){
+  $('.progressbar li').click( function() {
+  $(this).addClass('active').siblings().removeClass('active');
+});
+  
+});
+
+
+function openTab(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
